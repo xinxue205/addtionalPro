01b0aa4620b8218a74c3aa28e1e5d32074a95ed1
wuxinxue <xinxue205@126.com>
2020-09-15 13:45:32 +0800

    websocket测试

--- addtional_pro/js/websocket2.js
+++ addtional_pro/js/websocket2.js
@@ -7,29 +7,38 @@ var wss = new WebSocketServer({
     port: 8181
 }); //服务端口8181
 var speed = {//transid-[inputspeed, outspeed]
-    "A11111": [1, 2],
-    "A22222": [1, 2],
+    "A11111": {"inputSpeed": 1000, "outSpeed": 800 },
+    "A22222": {"inputSpeed": 1000, "outSpeed": 800 },
 }
 
 var count = 0;
 var randomSpeedUpdater = function () {
-    count++;
-    for (var item in speed) {
-        // if(item != 'isExec'){
-            var randomizedChange = parseInt(Math.random() * 10+1);//Math.random(60, 120);
-            if(Math.random() * 10>4){
-                speed[item][0] += randomizedChange;
-                speed[item][1] += randomizedChange;
-            } else {
-                speed[item][0] -= randomizedChange;
-                speed[item][1] -= randomizedChange;
+    try {
+        count++;
+        for (var item in speed) {
+            // if(item != 'isExec'){
+                var inChange = parseInt(Math.random() * 100);//Math.random(60, 120);
+                var outChange = parseInt(Math.random() * 100);//Math.random(60, 120);
+                if(Math.random() * 10>4){
+                    speed[item]["inputSpeed"] += inChange;
+                    speed[item]["outSpeed"] += outChange;
+                } else {
+                    speed[item]["inputSpeed"] -= inChange;
+                    speed[item]["outSpeed"] -= outChange;
+                }
+            // }
+            if(count%9==0){
+                if(speed.A11111){
+                    delete speed.A11111;
+                } else {
+                    speed.A11111 = {"inputSpeed": 1000, "outSpeed": 800 }
+                }
             }
-        // }
+        }
+        console.log(new Date()+" 更新数据:", JSON.stringify(speed));
+    } catch (err) {
+        console.log(err);
     }
-    // if(count%10==0){
-    //     speed['isExec'] = !speed['isExec'] ;
-    // }
-    console.log("更新数据:", JSON.stringify(speed));
 }
 //模拟车速实时变化
 
@@ -49,40 +58,48 @@ wss.on('connection', function (ws) {
     var currID = clientID ++;
     ws.id = currID;
     var sendSpeedUpdates = function (ws) {
-        if (ws.readyState == 1) {
-            var speedObj = speed[clientMap[currID]];
-            // for (var i = 0; i < clientSpeeds.length; i++) {
-            //     var item = clientSpeeds[i];
-            //     if(!speed[item]){
-            //         speed[item] = 0;
-            //     }
-            //     speedObj[item] = speed[item];
-            // }
-            if (speedObj) {
-                ws.send(JSON.stringify(speedObj)); //需要将对象转成字符串。WebSocket只支持文本和二进制数据,推送消息
-                console.log("服务器给客户端["+ currID +"]推送数据：", JSON.stringify(speedObj));
+        try {
+            if (ws.readyState == 1) {
+                var transid = clientMap[currID];
+                if (transid) {
+                    var speedObj = speed[transid]
+                    if(speedObj){
+                        speedObj.isExec = true;
+                        speedObj.inputSpeed = speedObj.inputSpeed < 0 ? 0 : speedObj.inputSpeed;
+                        speedObj.outSpeed = speedObj.outSpeed < 0 ? 0 : speedObj.outSpeed;
+                    } else {
+                        speedObj = {'isExec': false};
+                    }
+                    ws.send(JSON.stringify(speedObj)); //需要将对象转成字符串。WebSocket只支持文本和二进制数据,推送消息
+                    console.log("服务器给客户端["+ currID +"]推送数据：", JSON.stringify(speedObj));
+                }
             }
-
+        } catch (err) {
+            console.log(err);
         }
     }
 
     //每三秒发送一次
-    var clientSpeedUpdater = setInterval(function () {
-        sendSpeedUpdates(ws);
-    }, 3000);
+    var clientSpeedUpdater;
     ws.on('message', function (message) {
         var stockRequest = JSON.parse(message); //根据请求过来的数据来更新。
         console.log("服务器收到数据：", JSON.stringify(stockRequest));
-//        clientSpeeds = stockRequest['speed'];
-        clientMap[currID] = stockRequest.transid;
-        if(!speed[stockRequest.transid]){
-            speed[stockRequest.transid] = [1, 1];
+        if(clientMap[currID]){//已监听,先取消之前的定时推送
+            clearInterval(clientSpeedUpdater);
         }
-        //sendSpeedUpdates(ws);
+        clientMap[currID] = stockRequest.transid;
+        if(speed[stockRequest.transid]){//有速度，则启动定时推送
+            clientSpeedUpdater = setInterval(function () {
+                sendSpeedUpdates(ws);
+            }, 5000);
+        } else {//无速度，回复一次
+            speedObj = {isExec : false}
+            ws.send(JSON.stringify(speedObj)); //需要将对象转成字符串。WebSocket只支持文本和二进制数据,推送消息
+            console.log("服务器给客户端["+ currID +"]推送数据：", JSON.stringify(speedObj));
+        }
     });
     ws.on('close', function () {
         console.log("客户端["+ currID +"]断开连接");
-        delete speed[clientMap[currID]];
         delete clientMap[currID];
         if (clientSpeedUpdater) {
             //断开连接清楚定时器
